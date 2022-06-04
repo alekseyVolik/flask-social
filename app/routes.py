74df360b7ec926d08ff3743668f0a299732dfb92
Aleksey_Volik <volikjob@gmail.com>
2022-06-04 17:13:12 +0400

    Update 'index' view function
    According pagination feature we are added 'next_url' and 'prev_url' that send to template to creating links for pages

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,14 @@ def index():
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False
     )
-    return render_template('index.html', title='Home', posts=posts.items, form=form)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html',
+                           title='Home',
+                           posts=posts.items,
+                           form=form,
+                           next_url=next_url,
+                           prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
